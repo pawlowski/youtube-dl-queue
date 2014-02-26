fd2054a559278dceee100b6754020454fd9e4e6e
Peter Pawlowski <pnp@andrew.cmu.edu>
2014-02-26 11:13:17 -0500

    Save URLs to a temporary file.

--- enqueue.sh
+++ enqueue.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# run this program and paste URLs of YouTube, Vimeo, etc. 
+# and they will get downloaded automatically.
+
+# start the downloader:
+#./downloader.sh
+
+# wait for input
+QUEUE_DIR="$(dirname $0)/queue"
 while read u ; do
-  echo $u
+  queue_file=$(mktemp --tmpdir="${QUEUE_DIR}" "url.$(date +%Y%m%d).XXXXXX")
+  echo "${u}" > "${queue_file}"
 done
